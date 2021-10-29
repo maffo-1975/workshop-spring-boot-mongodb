e024d48131ecbbb73d7c8241f8ee35a5c8acbec1
Marcio Cavalcante <mca19ntl@gmail.com>
2021-10-29 09:05:22 -0300

    Obtendo um post por id

---
+++ src/main/java/com/marcioazevedo/workshopmongo/services/PostService.java
@@ -0,0 +1,23 @@
+package com.marcioazevedo.workshopmongo.services;
+
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.marcioazevedo.workshopmongo.domain.Post;
+import com.marcioazevedo.workshopmongo.repository.PostRepository;
+import com.marcioazevedo.workshopmongo.services.exception.ObjectNotFoundException;
+
+@Service
+public class PostService {
+
+	@Autowired
+	private PostRepository repo;
+
+	public Post findById(String id) {
+		Optional<Post> obj = repo.findById(id);
+		return obj.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado"));
+	}
+
+}
